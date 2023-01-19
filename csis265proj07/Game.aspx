555200a16626df2599db218ea36b21d1b079d463
Bryce Bohlman <S1457378@student.mcckc.edu>
2023-01-19 06:05:20 -0600

    Adding all project files

---
+++ csis265proj07/Game.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Game.aspx.cs" Inherits="csis265proj07.Game" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <br />
+        <asp:Label ID="lblInstruction1" runat="server" Text="Guess a Random Number Within 7 Tries"></asp:Label>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:Label ID="lblInstruction2" runat="server" Text="Enter your guess:  "></asp:Label>
+        <asp:TextBox ID="txtGuessInput" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Label ID="lblInputResponse" runat="server"></asp:Label>
+    </p>
+    <p>
+        <asp:Button ID="btnNewGame" runat="server" OnClick="btnNewGame_Click" Text="NewGame" />
+        <asp:Button ID="btnSubmitGuess" runat="server" OnClick="btnSubmitGuess_Click" Text="SubmitGuess" />
+    </p>
+</asp:Content>
---
+++ csis265proj07/Game.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace csis265proj07
+{
+    public partial class Game : System.Web.UI.Page
+    {
+        private Random generator;
+        private int guessCount;
+        private int winningNumber;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            generator = new Random();
+ 
+            if(!Page.IsPostBack)
+            {
+                guessCount = 0;
+                Session["guessCount"] = guessCount;
+                winningNumber = generator.Next(100);
+                Session["winningNumber"] = winningNumber;
+            }
+        }
+
+        protected void btnNewGame_Click(object sender, EventArgs e)
+        {
+            guessCount = 0;
+            Session["guessCount"] = guessCount;
+
+            winningNumber = generator.Next(100);
+            Session["winningNumber"] = winningNumber;
+
+            lblInputResponse.Text = string.Empty;
+        }
+
+        protected void btnSubmitGuess_Click(object sender, EventArgs e)
+        {
+            guessCount = (int)Session["guessCount"];
+            guessCount++;
+            Session["guessCount"] = guessCount;
+
+            winningNumber = (int)Session["winningNumber"];
+
+            if(Convert.ToInt32(txtGuessInput.Text) == winningNumber)
+            {
+                lblInputResponse.Text = $"Your guess is correct! ({guessCount} tries)";
+            }
+            else if (guessCount < 7)
+            {
+                if(Convert.ToInt32(txtGuessInput.Text) > winningNumber)
+                {
+                    lblInputResponse.Text = $"({guessCount} tries) Your guess is too high";
+                }
+                else 
+                {
+                    lblInputResponse.Text = $"({guessCount} tries) Your guess is too low";
+                }
+            }
+            else
+            {
+                lblInputResponse.Text = $"You did not guess {winningNumber} within 7 tries";
+            }
+
+            txtGuessInput.Text = string.Empty;
+        }
+    }
+}
